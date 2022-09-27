4bfe1ce37569b0cec81521e9170f8a7541ad3998
raflirahman <raflirahman375@gmail.com>
2022-09-27 11:35:25 +0700

    add tugas 7

---
+++ tugas7.js
@@ -0,0 +1,8 @@
+var angka = [170,171,172,173,174,175,176,178,179,180]
+for (let index = 0; index < angka.length; index++) {
+  console.log(angka[index]);
+}
+console.log("");
+for (const x of angka) {
+    console.log(x);
+}
